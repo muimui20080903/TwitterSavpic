018201d0818a0a005b70fc8edbdf79ce4d4be789
mui <hiro82585@gmail.com>
2023-03-24 16:28:22 +0900

    メディアが画像かどうかの判定とローカルへの画像保存を追加

--- openAI01.js
+++ openAI01.js
@@ -6,11 +6,10 @@ const { Client } = require('twitter-api-sdk');
 const client = new Client(env.Bearer_Token);
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("./test.db")//データベースのファイル名はあとで書き換える
-let sql ; // DBの操作は変数使うのがいいらしい、理由は英語でわかんなかった
+let sql; // DBの操作は変数使うのがいいらしい、理由は英語でわかんなかった
 //画像の保存
 const fs = require("fs");
-//const axios = require("axios");
-//const req = require("request");
+const axios = require("axios");
 
 //ユーザーの指定
 const userName = json[0]["screen_name"];
@@ -48,31 +47,34 @@ const params = {
               position: index,
             };
             results.push(result);
-            
-            //resultがSQliteの載ってるかを確認
+            if (result.url != undefined) {
+              //mediaが画像かどうか(gif,animationはurl取得できないから弾く)
+              //console.log("mediaを確認");
 
-            //新規のデータであった場合resultをSQliteに格納
-            /*
-            sql = "INSERT INTO twitterpic(author_name, aouthor_id, tweet_id, media_url, position, file_name) VALUES(?,?,?,?,?,?)";
-            db.run(sql,[],(err)=>{
-              if(err) return console.log(err.message);
-            });
-            */
-            //画像を保存
-            /*
-            req(
-              { method: "GET", url: media.url, encoding: null },
-              function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                  fs.writeFileSync(env.FILE_PATH_ROOT / tweet.id + "-" + index, body, "binary");
-                }
-              }
-            );*/
-            
+              //resultがすでにSQliteに載ってるかを確認
+              
+
+              //新規のデータであった場合resultをSQliteに格納
+              /*
+              //resulet.usernameが取得でき次第author_nameとfile_nameの変更
+              sql = "INSERT INTO twitterpic(author_name, author_id, tweet_id, media_url, position, file_name) VALUES(?,?,?,?,?,?)";
+              db.run(sql, [result.author_id, result.author_id, result.tweetId, result.url, result.position, "-"+result.tweetId + "-" + result.position], (err) => {
+                if (err) return console.log(err.message);
+              });*/
+
+              //画像を保存
+              const main = async () => {
+                const res = await axios.get(result.url, { responseType: "arraybuffer" });
+                fs.writeFileSync(env.FILE_PATH_ROOT + result.tweetId + "-" + result.position + ".png", new Buffer.from(res.data), "binary");
+                //console.log("保存完了");
+              };
+              main();
+
+            };
           }
         }
       }
-      console.log(results);
+      //console.log(results);
 
     })
     .catch((error) => {
