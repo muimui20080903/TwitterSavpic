94884f80a584531ead7c000ec177bc42a2da3a2f
mui <hiro82585@gmail.com>
2023-03-24 15:04:18 +0900

    dbの処理追加

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # TwitterSavpic
+Twitterのいいね欄から画像を保存する
+
+# セッティング
+`target.json`に
+```
+[
+    {
+        "screen_name":"mui",
+        "id":"9999999999"
+    },
+    {
+        "screen_name":"aiueo",
+        "id":""
+    },
+    {
+        "screen_name":"",
+        "id":"11111111"
+    }
+]
+```
+`.env`に
+```
+Bearer_Token="AAAAAAAAAAAAAAAAAA"
+```
--- openAI01.js
+++ openAI01.js
@@ -6,6 +6,11 @@ const { Client } = require('twitter-api-sdk');
 const client = new Client(env.Bearer_Token);
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("./test.db")//データベースのファイル名はあとで書き換える
+let sql ; // DBの操作は変数使うのがいいらしい、理由は英語でわかんなかった
+//画像の保存
+const fs = require("fs");
+//const axios = require("axios");
+const req = require("request");
 
 //ユーザーの指定
 const userName = json[0]["screen_name"];
@@ -14,7 +19,7 @@ const params = {
   'tweet.fields': 'created_at',
   'expansions': 'attachments.media_keys,author_id',
   'media.fields': 'url',
-  'user.fields': 'username',//←これよくわからん
+  //'user.fields': 'username',//←これよくわからん
 };
 
 (async () => {
@@ -22,14 +27,14 @@ const params = {
   const { data } = await client.users.findUserByUsername(userName);
   //console.log(data.id)
 
-//以降openchatくんに書いてもらったからよくわかってない
+  //以降openchatくんに書いてもらったからよくわかってない
   client.tweets.usersIdLikedTweets(data.id, params)
     .then((response) => {
       const tweets = response.data;
       const includes = response.includes;
 
       const results = [];
-      
+
       for (const tweet of tweets) {
         if (tweet.attachments && tweet.attachments.media_keys) {
           for (const [index, mediaKey] of tweet.attachments.media_keys.entries()) {
@@ -43,13 +48,26 @@ const params = {
               position: index,
             };
             results.push(result);
+            
+            //resultがSQliteの載ってるかを確認
 
-            //resultsをSQliteに格納
+            //新規のデータであった場合resultをSQliteに格納
             /*
-            db.serialize(() => {
-              db.run("insert into test_twitter(media_url,position,tweet_ID,author_ID) values(medis.url,index,tweet.id,tweet.author_id)");
+            sql = "INSERT INTO twitterpic(author_name, aouthor_id, tweet_id, media_url, position, file_name) VALUES(?,?,?,?,?,?)";
+            db.run(sql,[],(err)=>{
+              if(err) return console.log(err.message);
             });
-            db.close();*/
+            */
+            //画像を保存
+            req(
+              { method: "GET", url: media.url, encoding: null },
+              function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                  fs.writeFileSync(env.FILE_PATH_ROOT / tweet.id + "-" + index, body, "binary");
+                }
+              }
+            );
+            
           }
         }
       }
